3cbe7eb24ab8d04601a571865821ab0468f97073
dnolen <dnolen@cognitect.com>
2015-03-27 16:18:55 -0400

    latest

--- src/jsx_fun/core.clj
+++ src/jsx_fun/core.clj
@@ -1,26 +1,29 @@
 (ns jsx-fun.core
   (:require [clojure.java.io :as io])
-  (:import (javax.script ScriptEngineManager Invocable)
-           (java.io FileReader)
-           (jdk.nashorn.api.scripting ScriptObjectMirror)))
+  (:import [javax.script ScriptEngineManager]))
 
-(def eng (.getEngineByName (ScriptEngineManager.) "nashorn"))
+(defn jsx-engine []
+  (let [nashorn (.getEngineByName (ScriptEngineManager.) "nashorn")]
+    (doto nashorn
+      (.eval "var global = {};")
+      (.eval (io/reader (io/resource "com/facebook/jsx.js"))))))
+
+(defn transform
+  [jsx-eng src dest]
+  (let [options
+        (.eval jsx-eng
+          "(function(){ return {stripTypes: true, harmony: true};})()")
+        jsxt (.eval jsx-eng "global.JSXTransformer")]
+    (spit (io/file dest)
+      (.get
+        (.invokeMethod jsx-eng jsxt "transform"
+          (object-array [(slurp src) options]))
+        "code"))))
 
 (comment
-  (.eval eng "var global = {};")
-  (.eval eng "")
-  (.eval eng (io/reader (io/resource "com/facebook/jsx.js")))
+  (def jsx-eng (jsx-engine))
 
-  (def jsx (.eval eng "global.JSXTransformer"))
-  (def options (.eval eng "(function(){ return {stripTypes: true, harmony: true};})()"))
-
-  (.invokeMethod eng jsx "transform"
-    (object-array
-      [(slurp (io/resource "ScrollResponder.js")) options]))
-
-  (def json (.eval eng "JSON"))
-  (.invokeMethod eng json "stringify" (object-array [options]))
-
-  (.invokeMethod eng json "stringify" (object-array [1]))
-
+  (transform jsx-eng
+    (io/file "resources/ScrollResponder.js")
+    (io/file "resources/ScrollResponder.out.js"))
   )
