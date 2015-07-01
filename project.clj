79be0907baea34c1a2a6208f10260ec3109faad6
dnolen <dnolen@cognitect.com>
2015-07-01 15:36:28 -0400

    bump deps move things around

--- project.clj
+++ project.clj
@@ -3,9 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3165" :exclusions
-                  [com.google.javascript/closure-compiler]]
-                 [com.google.javascript/closure-compiler "1.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "0.0-3423"]
                  [com.facebook/jsx "0.13.1"]]
   :plugins [[cider/cider-nrepl "0.8.2"]])
